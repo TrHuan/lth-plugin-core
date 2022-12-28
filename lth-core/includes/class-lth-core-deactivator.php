7045495ba09f2b32438bd2e10aa6bcc3f3534442
TrHuan <trhuan177@gmail.com>
2022-12-28 10:50:58 +0700

    Version 2.0.0

---
+++ lth-core/includes/class-lth-core-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://http://lth90.42web.io/
+ * @since      2.0.0
+ *
+ * @package    Lth_Core
+ * @subpackage Lth_Core/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      2.0.0
+ * @package    Lth_Core
+ * @subpackage Lth_Core/includes
+ * @author     LTH <trhuan177@gmail.com>
+ */
+class Lth_Core_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    2.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
