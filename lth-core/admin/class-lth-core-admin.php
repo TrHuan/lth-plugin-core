1a73c851f9335d441e2df4b4c93be032015df32f
TrHuan <trhuan177@gmail.com>
2022-12-28 14:19:54 +0700

    up code

--- lth-core/admin/class-lth-core-admin.php
+++ lth-core/admin/class-lth-core-admin.php
@@ -4,7 +4,7 @@
  * The admin-specific functionality of the plugin.
  *
  * @link       https://http://lth90.42web.io/
- * @since      1.0.0
+ * @since      2.0.0
  *
  * @package    Lth_Core
  * @subpackage Lth_Core/admin
@@ -25,7 +25,7 @@ class Lth_Core_Admin {
 	/**
 	 * The ID of this plugin.
 	 *
-	 * @since    1.0.0
+	 * @since    2.0.0
 	 * @access   private
 	 * @var      string    $plugin_name    The ID of this plugin.
 	 */
@@ -34,7 +34,7 @@ class Lth_Core_Admin {
 	/**
 	 * The version of this plugin.
 	 *
-	 * @since    1.0.0
+	 * @since    2.0.0
 	 * @access   private
 	 * @var      string    $version    The current version of this plugin.
 	 */
@@ -43,7 +43,7 @@ class Lth_Core_Admin {
 	/**
 	 * Initialize the class and set its properties.
 	 *
-	 * @since    1.0.0
+	 * @since    2.0.0
 	 * @param      string    $plugin_name       The name of this plugin.
 	 * @param      string    $version    The version of this plugin.
 	 */
@@ -57,7 +57,7 @@ class Lth_Core_Admin {
 	/**
 	 * Register the stylesheets for the admin area.
 	 *
-	 * @since    1.0.0
+	 * @since    2.0.0
 	 */
 	public function enqueue_styles() {
 
@@ -80,7 +80,7 @@ class Lth_Core_Admin {
 	/**
 	 * Register the JavaScript for the admin area.
 	 *
-	 * @since    1.0.0
+	 * @since    2.0.0
 	 */
 	public function enqueue_scripts() {
 
--- lth-core/includes/class-lth-core-i18n.php
+++ lth-core/includes/class-lth-core-i18n.php
@@ -7,7 +7,7 @@
  * so that it is ready for translation.
  *
  * @link       https://http://lth90.42web.io/
- * @since      1.0.0
+ * @since      2.0.0
  *
  * @package    Lth_Core
  * @subpackage Lth_Core/includes
@@ -19,7 +19,7 @@
  * Loads and defines the internationalization files for this plugin
  * so that it is ready for translation.
  *
- * @since      1.0.0
+ * @since      2.0.0
  * @package    Lth_Core
  * @subpackage Lth_Core/includes
  * @author     LTH <trhuan177@gmail.com>
@@ -30,7 +30,7 @@ class Lth_Core_i18n {
 	/**
 	 * Load the plugin text domain for translation.
 	 *
-	 * @since    1.0.0
+	 * @since    2.0.0
 	 */
 	public function load_plugin_textdomain() {
 
